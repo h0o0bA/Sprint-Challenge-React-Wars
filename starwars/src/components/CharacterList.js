446695b35e4cc221ba40768d4a4e416a598e38c6
Ihab Ali <ihab@live.com>
2019-05-19 16:42:23 -0500

    added Character.js and CharacterList.js

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Character from './Character'
+import './StarWars.css';
+
+
+const CharacterList = props => { //props is data
+    console.log(props.starwarsChars) //is it working?
+    return (
+
+        <div className="char-wrapper">
+            {props.starwarsChars.map( character => (
+                <Character key={character.name}
+                characterData={character}/>
+
+            ))};
+        </div>
+
+
+
+
+    );
+};
+
+
+
+export default CharacterList;
